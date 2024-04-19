ed5584ddc3e856a0708fb9609630d5927585b7ce
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-04-20 00:34:18 +0100

    feat:

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        // menú asociado a la clase Menu.java
         ArrayList<Restaurante> restaurantes = new ArrayList<>();
         Menu menu = new Menu(restaurantes);
         menu.mostrarMenu();
